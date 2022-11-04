f88cc5e30f89035af2597a993f6fadfed92f9f0c
petyappetrov <p.petrov@indriver.com>
2022-11-04 15:45:02 +0600

    feat: Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://petyappetrov.com',
+  site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
