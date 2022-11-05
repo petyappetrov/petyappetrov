84f5d52fe5eeacf8201050d200730481a9ca2c30
petyappetrov <p.petrov@indriver.com>
2022-11-05 18:49:33 +0600

    feat: SPA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// // https://astro.build/config
-// import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://petyappetrov.com',
