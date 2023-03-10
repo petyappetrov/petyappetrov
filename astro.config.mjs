1d7ea5bff54570b9a51f8b4f70ff1c8ae0e6ecb8
petyappetrov <p.petrov@indriver.com>
2023-03-10 10:12:11 +0600

    feat: Correct astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://petyappetrov.com/',
-  integrations: [mdx(), sitemap()]
+  site: "https://petyappetrov.com/",
+  integrations: [mdx(), sitemap(), compress()],
 });
